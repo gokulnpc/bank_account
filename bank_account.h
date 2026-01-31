1126dfc2ffb8c3a13ea72a335b4876c9f8af1b62
Gokuleshwaran Narayanan <gokulnpc@gmail.com>
2026-01-31 16:59:38 -0500

    q1 commit

--- bank_account.h
+++ bank_account.h
@@ -1,6 +1,7 @@
 // bank_account.h - Simle bank account class definition
 #pragma once
 #include <stdexcept>
+#include <limits>
 
 class BankAccount {
     // private class member not visible outside the class
@@ -24,6 +25,9 @@ public:
     constexpr unsigned withdraw(unsigned amount)
     {
 		// TODO: throw exception if amount > _balance
+        if (amount > _balance) {
+            throw std::runtime_error("insufficient funds");
+        }
         _balance -= amount;
 
         return _balance;
@@ -31,6 +35,9 @@ public:
     constexpr unsigned deposit(unsigned amount)
     {
 		// TODO: throw exception if addition would cause overflow
+        if (amount > std::numeric_limits<unsigned>::max() - _balance) {
+            throw std::runtime_error("balance overflow");
+        }
         _balance += amount;
 
         return _balance;
